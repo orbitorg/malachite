63ed42580d336277ded20ce70e19ead6aa10b6f4
Romain Ruetschi <romain@informal.systems>
2024-11-01 01:38:47 +0100

    feat(code): Add persistence to the block store

--- code/crates/starknet/host/src/part_store.rs
+++ code/crates/starknet/host/src/part_store.rs
@@ -54,7 +54,7 @@ impl<Ctx: Context> PartStore<Ctx> {
         self.store.retain(|(height, _), _| *height >= min_height);
     }
 
-    pub fn blocks_stored(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.store.len()
     }
 }
