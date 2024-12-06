79e905c81014b4c6b5326a4606c1806588d033bc
Adi Seredinschi <a@seredinschi.net>
2024-12-06 09:48:39 +0100

    part 2 of rename

--- code/examples/basehost/src/common.rs
+++
@@ -1,6 +0,0 @@
-use malachite_metrics::{Metrics, Registry, SharedRegistry};
-
-pub fn new_metrics() -> Metrics {
-    let registry = SharedRegistry::new(Registry::default());
-    Metrics::register(&registry)
-}
